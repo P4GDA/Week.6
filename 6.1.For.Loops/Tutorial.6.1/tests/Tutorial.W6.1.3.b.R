6c083077d7619477fb096848345b9ac5184721d6
Heather Gibling <heathergibling@gmail.com>
2022-02-03 15:34:20 -0500

    update formatting

--- 6.1.For.Loops/Tutorial.6.1/tests/Tutorial.W6.1.3.b.R
+++ 6.1.For.Loops/Tutorial.6.1/tests/Tutorial.W6.1.3.b.R
@@ -6,7 +6,7 @@ test = list(
       name = NA,
       points = 1.0,
       code = {
-        expect_equal(round(perc_pathogenic_variants, 6), c("cystic.fibrosis"=37.403001, "duchenne.muscular.dystrophy"=28.038777, "glaucoma"=3.846154, "pheynlketonuria"=60.517435, "type.2.diabetes"=21.126761))
+        expect_equal(round(percent_pathogenic_variants, 6), c("cystic.fibrosis"=37.403001, "duchenne.muscular.dystrophy"=28.038777, "glaucoma"=3.846154, "pheynlketonuria"=60.517435, "type.2.diabetes"=21.126761))
       }
     )
   )
